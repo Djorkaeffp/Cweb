bac66ca242e5b8728e435c582fbf7537ce6f088d
Djorkaeffp <djorkaeffoliveira7@gmail.com>
2026-01-19 20:57:45 -0300

    vercel

--- pweb/settings.py
+++ pweb/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parser{'postgresql://neondb_owner:npg_2Cmc5hTaIMeK@ep-small-cherry-acdbthui-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require
+'}
 }
 
 
